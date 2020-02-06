bea14e2b640b9579b2b5043fe9117e85fc7beec6
JORGE GABRIEL RICCI DUTRA <19266852@CFP127ALUNO.LOCAL>
2020-02-06 11:25:35 -0300

    refatorando e utilizando arrow function

--- Aulas1/script.js
+++ Aulas1/script.js
@@ -28,30 +28,70 @@ console.log(++nota2);
 
 nota += 1;
 */
+
+
 function chama() {
+    
+    // DECLARANDO AS VARIÁVEIS
     const $nome = window.document.getElementById("nome").value;
     const $nota1 = window.document.getElementById("nota1").value;
     const $nota2 = window.document.getElementById("nota2").value;
     const $media = window.document.getElementById("media");
     const $situacao = window.document.getElementById("situacao");
+    const $conceito = window.document.getElementById("conceito");
 
     $situacao.style.textAlign = "center";
     $situacao.style.color = "white";
 
-    const nota1 = Number($nota1);
-    const nota2 = Number($nota2);
 
-    $media.value = ( nota1 + nota2 ) / 2;
+    if ($nota1 != "" && nota2 != "") {
 
-    if ($media.value >= 5 ) {
-        $situacao.value = "APROVADO";
-        $situacao.style.backgroundColor = "green"
+        const nota1 = Number($nota1);
+        const nota2 = Number($nota2);
+
+        $media.value = calcularMedia(nota1, nota2);
+        $situacao.value = verificaSituacao($media.value, $situacao);
+        $conceito.value = mostrarConceito($media.value);
 
     } else {
-        $situacao.value = "REPROVADO";
-        $situacao.style.backgroundColor = "red";
+        window.alert("LIDIA RESOLVI");
+
+    }
+    
+}
+
+
+const calcularMedia = (nota1, nota2) => ( nota1 + nota2 ) / 2;
+
+
+const mostrarConceito = media => {
+    if (media >= 0 && media < 3) {
+        return "E";
+
+    } else if (media >= 3 && media < 5) {
+        return "D";
+
+    } else if (media >=5 && media < 8) {
+        return "C";
+
+    } else if (media >= 8 && media < 10) {
+        return "B";
+
+    } else if (media == 10) {
+        return "A";
+
+    }
+}
+
+
+const verificaSituacao = (media, css) => {
+    if (media >= 5 ) {
+        css.style.backgroundColor = "green";
+        return "APROVADO";
+        
+    } else {
+        css.style.backgroundColor = "red";
+        return "REPROVADO";
         
     }
-
-    
 }
