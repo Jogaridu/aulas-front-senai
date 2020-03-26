342c1c58c266f85ee16a5b17f87e400f20565087
Jorge Gabriel <e-jorge2010@hotmail.com>
2020-03-26 14:55:40 -0300

    Postmon API

---
+++ Aulas7-Postmon/readme.md
@@ -0,0 +1,13 @@
+## Exercicio proposto para consumir uma API
+
+## Capacidade
+Utilizar uma API para buscar dados em uma outra URL
+
+## Conhecimentos em JavaScript
+- API Fetch
+- Promisses
+- Métodos como then() e json()
+- Eventos do JavaScript "blur", "keyup"
+- Validar entrada de dados
+- Máscaras para entrada de dados
+- Async e Await
---
+++ Aulas7-Postmon/viacep.js
@@ -0,0 +1,56 @@
+"use stritc";
+
+const verificarCep = () => {
+    return document.getElementById("cep").reportValidity();
+}
+
+const encontrarCep = async (cep) => {
+
+    if (verificarCep()) {
+        const url = `https://api.postmon.com.br/v1/cep/${cep}`
+
+        // 1° Primeiro usamos a API FETCH e passa como parâmetro uma URL
+        // 2° Segundo usa o método then(callback) para mostrar o resultado da URL
+        // 3° Terceiro transforma a resposta (res) em um JSON
+        // 4° Quarto usa o método then() novamente para pegar os dados do JSON
+
+        // Método sem o async e o await
+        // fetch(url).then( (res) => res.json().then((dados) => preencherFormulario(dados)),
+        //                  () => window.alert("CEP Inválido"));
+
+        document.getElementById("endereco").value = "Pesquisando CEP..."
+
+        // Método com o async e o await
+        const pegarEndereco = await fetch(url);
+        const endereco = await pegarEndereco.json()
+        preencherFormulario(endereco)
+        
+    }
+    
+}
+
+const preencherFormulario = (endereco) => {
+    if (endereco.erro) {
+        document.getElementById("endereco").value = "";
+        document.getElementById("bairro").value = "";
+        document.getElementById("cidade").value = "";
+        document.getElementById("estado").value = "";
+
+    } else {
+        document.getElementById("endereco").value = endereco.logradouro;
+        document.getElementById("bairro").value = endereco.bairro;
+        document.getElementById("cidade").value = endereco.localidade;
+        document.getElementById("estado").value = endereco.uf;
+    }
+}
+
+const maskCep = ($el) => {
+    let aux = $el.value;
+    aux = aux.replace(/[^0-9]/g, "");
+    aux = aux.replace(/(.{5})(.)/, '$1-$2');
+    $el.value = aux;
+}
+
+const $cep = document.getElementById("cep");
+$cep.addEventListener("blur", () => encontrarCep($cep.value));
+$cep.addEventListener("keyup",(e) => maskCep (e.target))
